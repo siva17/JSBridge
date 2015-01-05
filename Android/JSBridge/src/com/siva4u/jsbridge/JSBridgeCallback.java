a94978e406e668cc0705ab4f31b3f326166f039e
Siva R <siva@siva4u.com>
2015-01-04 23:29:36 -0500

    Added Android also

---
+++ Android/JSBridge/src/com/siva4u/jsbridge/JSBridgeCallback.java
@@ -0,0 +1,7 @@
+package com.siva4u.jsbridge;
+
+import org.json.JSONObject;
+
+public interface JSBridgeCallback {
+	public void callBack(JSONObject data);
+}
---
+++ Android/JSBridge/src/com/siva4u/jsbridge/JSBridgeHandler.java
@@ -0,0 +1,8 @@
+package com.siva4u.jsbridge;
+
+import org.json.JSONObject;
+
+public interface JSBridgeHandler {
+	public void hanlder(JSONObject data, JSBridgeCallback responseCallback);
+
+}
